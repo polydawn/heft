3b245da2021ee980c35826993889f8f21ce01214
Eric Myhre <hash@exultant.us>
2018-02-24 16:46:10 -0700

    Move interpreter setup to package.
    Test cases are going to be needed to progress much futher.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/heft/main.go
+++ cmd/heft/main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	sk "github.com/google/skylark"
-
-	"go.polydawn.net/heft/skyform"
+	"go.polydawn.net/heft/interpret"
 )
 
 func main() {
-	execfile(os.Args[1])
-}
-
-// Makes a new globals dict with our favorite custom bits in it.
-func newGlobals() sk.StringDict {
-	return sk.StringDict{
-		"iamheft": sk.NewBuiltin("iamheft", func(thread *sk.Thread, fn *sk.Builtin, args sk.Tuple, kwargs []sk.Tuple) (sk.Value, error) {
-			if thread.Print != nil {
-				thread.Print(thread, "yes")
-			} else {
-				fmt.Fprintln(os.Stderr, "yes")
-			}
-			return sk.None, nil
-		}),
-		"formula":       sk.NewBuiltin("formula", skyform.MakeFormulaUnion),
-		"basting":       sk.NewBuiltin("basting", skyform.NewBasting),
-		"releaseItemID": sk.NewBuiltin("releaseItemID", skyform.NewReleaseItemID),
-	}
-}
-
-func execfile(filename string) {
-	thread := &sk.Thread{}
-	globals := newGlobals()
-	if err := sk.ExecFile(thread, filename, nil, globals); err != nil {
-		fmt.Fprintf(os.Stderr, "larking: %s\n", err)
-		os.Exit(4)
-	}
+	interpret.ExecFile(os.Args[1])
 }
