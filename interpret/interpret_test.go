a0372c47acf944ccae14442cb40a26b11a36dbd7
Eric Myhre <hash@exultant.us>
2018-03-03 21:59:05 -0800

    Tests based on simply checking the right globals come out.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- interpret/interpret_test.go
+++ interpret/interpret_test.go
@@ -2,7 +2,10 @@ package interpret
 
 import (
 	"fmt"
+	"sort"
 	"testing"
+
+	sk "github.com/google/skylark"
 )
 
 func TestHello(t *testing.T) {
@@ -10,6 +13,7 @@ func TestHello(t *testing.T) {
 	loader := Loader{}
 	globals, err := loader.EvalScript(script)
 	fmt.Printf(": %#v\n: %#v\n", globals, err)
+	mustGlobalKeys(t, globals, []string{}...)
 }
 
 func TestModuleHello(t *testing.T) {
@@ -24,4 +28,20 @@ def fwee():
 	script := `load ("fwee.sk", "fwee"); fwee()`
 	globals, err := loader.EvalScript(script)
 	fmt.Printf(": %#v\n: %#v\n", globals, err)
+	mustGlobalKeys(t, globals, "fwee")
+}
+
+func mustGlobalKeys(t *testing.T, globals sk.StringDict, wantKeys ...string) {
+	t.Helper()
+	keys := make([]string, 0, len(globals))
+	for k := range globals {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	sort.Strings(wantKeys)
+	keysStr := fmt.Sprintf("%#v", keys)
+	wantKeysStr := fmt.Sprintf("%#v", wantKeys)
+	if wantKeysStr != keysStr {
+		t.Fatalf("want keys: %#v\n got keys: %#v\n", wantKeysStr, keysStr)
+	}
 }
