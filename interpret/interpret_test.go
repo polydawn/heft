74da7ce2288b48d8c3bfec065563064522b66318
Eric Myhre <hash@exultant.us>
2018-03-03 22:38:03 -0800

    Whee, regression test for stacking formulas with the '+' operator!
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- interpret/interpret_test.go
+++ interpret/interpret_test.go
@@ -31,6 +31,42 @@ def fwee():
 	mustGlobalKeys(t, globals, "fwee")
 }
 
+func TestFormulaFold(t *testing.T) {
+	loader := Loader{}
+	script := `
+f1 = formula({
+	"formula":{"action":{
+		"exec":["wow", "-c", "as\ndf\n"],
+	}},
+})
+f2 = formula({
+	"formula":{"action":{
+		"env":{"VAR1":"bees"},
+	}},
+})
+f3 = formula({
+	"formula":{"action":{
+		"env":{"VAR2":"bats"},
+		"exec":["crash", "override"],
+	}},
+})
+f123=f1 + f2 + f3
+`
+	globals, err := loader.EvalScript(script)
+	if err != nil {
+		t.Fatalf("error interpreting: %v", err)
+	}
+	mustGlobalKeys(t, globals, "f1", "f2", "f3", "f123")
+	shouldStringEqual(t, `<FormulaUnion:{"formula":{"inputs":{},"action":{"exec":["crash","override"],"env":{"VAR1":"bees","VAR2":"bats"}},"outputs":{}}}>`, globals["f123"].String())
+}
+
+func shouldStringEqual(t *testing.T, expect, actual string) {
+	t.Helper()
+	if expect != actual {
+		t.Errorf("want string: %#v\n got string: %#v\n", expect, actual)
+	}
+}
+
 func mustGlobalKeys(t *testing.T, globals sk.StringDict, wantKeys ...string) {
 	t.Helper()
 	keys := make([]string, 0, len(globals))
