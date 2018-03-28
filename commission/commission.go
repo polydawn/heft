8df5f8806f7f8035d7d33bb9dccc37af26b42fe4
Eric Myhre <hash@exultant.us>
2018-03-28 17:02:56 +0200

    First pass on logistics for even bigger graphs.
    Basting, it turns out, is not enough.  It's useful to string small
    locally-grouped sets of formulas together for making moderately complex
    products (and making it possible to run their tests in separately
    isolated environments, lets not forget).  But a bigger picture remains.
    
    "Commissions" (working title, like much of this -- the nomenclature
    refactor date is probably not far off now) are meant to be this bigger
    picture.
    
    Commissions will be sort of like an ultra-basting: by looking at a
    group of bastings and doing a topological sort on their imports, we
    can build a DAG of which things would be evaluted if building the
    entire suite of bastings at once from their farthest-back sources.
    
    We're going to give this first class support: it will be possible to
    calculate the commission graph for an entire set of projects, and
    importantly, to do it *even without building anything*.  We're going
    to do this without significantly new data types; we can actually get
    everything to work within our existing types, and in fact without
    changing much of the model of "planners" being any ol' abstract piece
    of logic that can emit a basting.  By blackboxing the planner and
    controlling the presentation of release info it sees, we can actually
    check if the planner would accept hypothetical releases without
    knowing anything about the logic it uses internally to decide that.
    This will allow us to compute downstream splash radiuses of any changes
    anywhere in the commission graph; provide the whole commission graph
    and a toposort order of it to an evaluation agent; and all sorts of
    other excellent things like that.
    
    This also marks a change in thinking about where exactly the halting
    points in the system are.  Of course, release records will still be
    serializable points, and the replay instructions in them expected to
    be, well, totally capable of supporting replay of that build process.
    However, rather than visualizing our whole system as a bunch of basting
    processes connecting release cells, it's now more advisable to
    visualize the whole commission graph as a bunch of conjoined bastings,
    and the release cells are actually precipitants off of that graph.
    They're deeply related; but the commission graph exists before and
    independent of the release cells.  And it's purely "coincidental" that
    re-examining a huge heap of release records and their attached replay
    instructions later in tinfoil-hat audit mode will *happen* to draw up
    the same graph as the commission originally set out to build up.
    
    There's a LOT of "small matter of programming" between the present
    versus realizing that whole picture, of course.  This commit is just
    a fraction of the work ahead.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ commission/commission.go
@@ -0,0 +1,88 @@
+package commission
+
+import (
+	"fmt"
+	"strings"
+
+	"go.polydawn.net/go-timeless-api"
+)
+
+// CommissionGraph is a set of catalog/project names mapped to the set of
+// catalogs they import.
+//
+// REVIEW: it seems entirely possible we need a {catalog,version} tuple
+// in the set, because it is still allowed to select non-tip releases,
+// and hypothetically even more than one version from the same catalog.
+type CommissionGraph map[api.CatalogName]map[api.CatalogName]bool
+
+func commissionRecursively(loader CommissionTreeViewer, startAt api.CatalogName, visited CommissionGraph, backtrace []string) (CommissionGraph, error) {
+	// First, check for cycles.  If this is in our current walk path already, bad.
+	nBacktrace := len(backtrace)
+	backtrace = append(backtrace, string(startAt))
+	for i, backstep := range backtrace[:nBacktrace] {
+		if backstep == string(startAt) {
+			return visited, fmt.Errorf("cycle found: %s", strings.Join(backtrace[i:nBacktrace], " -> "))
+		}
+	}
+
+	// If we have visited this before (and not in a cycle), no-op; already have answer.
+	if _, ok := visited[startAt]; ok {
+		return visited, nil
+	}
+
+	// Load up and interpret the hitching script, then note the imports resulting.
+	hitching, err := loader.LoadSynthesis(startAt)
+	if err != nil {
+		return visited, err
+	}
+	basting, err := commission(*hitching)
+	if err != nil {
+		return visited, err
+	}
+	importSet := projectImportSet(*basting)
+	if visited == nil {
+		visited = make(CommissionGraph)
+	}
+	visited[startAt] = importSet
+
+	// We must now recurse through each of these new imports.
+	for imp := range importSet {
+		visited, err = commissionRecursively(loader, imp, visited, backtrace)
+		if err != nil {
+			return visited, err
+		}
+	}
+
+	// Now that all the recursing below us is done, pop our backtrace element.
+	backtrace = backtrace[:len(backtrace)-1]
+
+	return visited, nil
+}
+
+func commission(Hitching) (*api.Basting, error) {
+	// TODO invoke interpreter
+	return &api.Basting{}, nil
+}
+
+func projectImportSet(basting api.Basting) map[api.CatalogName]bool {
+	v := make(map[api.CatalogName]bool)
+	for _, step := range basting.Steps {
+		for _, imp := range step.Imports {
+			v[imp.CatalogName] = true
+		}
+	}
+	return v
+}
+
+type Hitching string // a skylark script
+
+type CommissionTreeViewer interface {
+	LoadSynthesis(api.CatalogName) (*Hitching, error)
+}
+
+type moduleFilesystem interface {
+}
+
+var _ moduleFilesystem = mockFs{}
+
+type mockFs map[string]string
